cb8ac5a68339cd8bf565d911259ee9e619a923df
Zach Brooks <kindest-samba0s@icloud.com>
2023-05-05 11:54:46 -0500

    Reformatted python code

--- src/modules/update_vpc_peering/update_vpc_peering.py
+++ src/modules/update_vpc_peering/update_vpc_peering.py
@@ -1,5 +1,4 @@
 from google.cloud import compute_v1
-import json
 import copy
 import time
 
@@ -16,12 +15,33 @@ class updateVpcPeering:
         self.peering_name = peering_name
         self.network = None
 
-
     def return_network(self) -> object:
-        self.network = self.client.get(project=self.project_id, network=self.network_name)
+        """Returns network for Google compute API which contains the VPC Peering
+
+        Args:
+            self (updateVpcPeering): Instance of the Class
+
+        Returns:
+            network (object): Returns Network Object
+
+        Raises:
+        """
+        self.network = self.client.get(
+            project=self.project_id, network=self.network_name
+        )
         return self.network
 
     def return_updates(self) -> object:
+        """Parses VPC Peerings and returns peering to update
+
+        Args:
+            self (updateVpcPeering): Instance of the Class
+
+        Returns:
+            update (object): Peering Object with updated attribute (export_custom_routes=True)
+        
+        Raises:
+        """
         peerings = self.network.peerings
         update = None
         for peering in peerings:
@@ -32,19 +52,56 @@ class updateVpcPeering:
         return update
 
     def update_peering(self, update: object) -> dict:
+        """Runs update_peering operation to add export_custom_routes=True to the peering
+
+        Args:
+            self (updateVpcPeering): Instance of the Class
+            update (object): Peering Object with updated attribute
+
+        Returns:
+            status (dict): Status object containing bool and msg
+        
+        Raises:
+        """
         try:
             request = compute_v1.NetworksUpdatePeeringRequest(network_peering=update)
-            response = self.client.update_peering(project=self.project_id, network=self.network_name, networks_update_peering_request_resource=request)
+            response = self.client.update_peering(
+                project=self.project_id,
+                network=self.network_name,
+                networks_update_peering_request_resource=request,
+            )
             success = self.check_status(response=response)
             if success:
-                status = {"status": success, "msg": f"Successfully updated peering {self.peering_name}..."}
+                status = {
+                    "status": success,
+                    "msg": f"Successfully updated peering {self.peering_name}...",
+                }
             else:
-                status = {"status": success, "msg": f"Failed to update peering {self.peering_name}..."}
+                status = {
+                    "status": success,
+                    "msg": f"Failed to update peering {self.peering_name}...",
+                }
         except Exception:
-            status = {"status": False, "msg": f"Failed to update peering {self.peering_name}..."}
+            status = {
+                "status": False,
+                "msg": f"Failed to update peering {self.peering_name}...",
+            }
         return status
 
     def check_status(self, response: object) -> bool:
+        """Checks the status of the response object from peering_update operation (async)
+        
+        Will execute done method within the class until True is returned or max_retries is exceeded
+
+        Args:
+            self (updateVpcPeering): Instance of the Class
+            response (object): Response object from peering operation
+
+        Returns:
+            status (bool): Bool indicating the operation finished
+        
+        Raises:
+        """
         max_retries = 5
         wait = 5
         status = False
@@ -56,17 +113,42 @@ class updateVpcPeering:
         return status
 
     def run_peering_update(self) -> dict:
+        """Runs peering update (entrypoint method)
+
+        Args:
+            self (updateVpcPeering): Instance of the Class
+
+        Returns:
+            status (dict): Status object containing bool and msg
+        
+        Raises:
+        """
         self.return_network()
         update = self.return_updates()
         if update:
             status = self.update_peering(update)
         else:
-            status = {"status": True, "msg": f"Peering {self.peering_name} already has export_custom_routes enabled..."}
+            status = {
+                "status": True,
+                "msg": f"Peering {self.peering_name} already has export_custom_routes enabled...",
+            }
         return status
 
+
 def update_peering(network_name: str, peering_name: str, project_id: str) -> dict:
+    """Runs peering update, wrapper function for updateVpcPeering class
+
+    Args:
+        network_name (str): Name of the VPC with the Peering
+        peering_name (str): Name of the GKE Peering to update
+        project_id (str): Project ID where network/peering exists
+
+    Returns:
+        status (dict): Status object containing bool and msg
+    
+    Raises:
     """
-    Function to execute peering update
-    """
-    status = updateVpcPeering(network_name=network_name, project_id=project_id, peering_name=peering_name).run_peering_update()
+    status = updateVpcPeering(
+        network_name=network_name, project_id=project_id, peering_name=peering_name
+    ).run_peering_update()
     return status
--- tests/test_update_vpc_peering.py
+++ tests/test_update_vpc_peering.py
@@ -2,19 +2,33 @@ from update_vpc_peering import update_vpc_peering
 from unittest.mock import MagicMock
 import pickle
 import unittest
-from mock import patch
 
 
 class TestupdateVpcPeering(unittest.TestCase):
-
     def setUp(self):
         with open("google_sdk_network_mock.data", "rb") as pfile:
             self.network_mock_data = pickle.load(pfile)
-        self.updateVpcPeering = update_vpc_peering.updateVpcPeering(project_id="fakeprojectid", network_name="test-vpc-name", peering_name="gke-ijsadoasoidasod-peer")
-        self.updateVpcPeering.return_network = MagicMock(return_value=self.network_mock_data)
-        self.updateVpcPeering.update_peering = MagicMock(return_value={"status": True, "msg": "Successfully updated peering gke-ijsadoasoidasod-peer..."})
+        self.updateVpcPeering = update_vpc_peering.updateVpcPeering(
+            project_id="fakeprojectid",
+            network_name="test-vpc-name",
+            peering_name="gke-ijsadoasoidasod-peer",
+        )
+        self.updateVpcPeering.return_network = MagicMock(
+            return_value=self.network_mock_data
+        )
+        self.updateVpcPeering.update_peering = MagicMock(
+            return_value={
+                "status": True,
+                "msg": "Successfully updated peering gke-ijsadoasoidasod-peer...",
+            }
+        )
         self.updateVpcPeering.check_status = MagicMock(return_value=True)
-        self.updateVpcPeering.run_peering_update = MagicMock(return_value={"status": True, "msg": "Successfully updated peering gke-ijsadoasoidasod-peer..."})
+        self.updateVpcPeering.run_peering_update = MagicMock(
+            return_value={
+                "status": True,
+                "msg": "Successfully updated peering gke-ijsadoasoidasod-peer...",
+            }
+        )
         self.updateVpcPeering.network = self.network_mock_data
 
     def test_return_network(self):
@@ -26,7 +40,7 @@ class TestupdateVpcPeering(unittest.TestCase):
         update = self.updateVpcPeering.return_updates()
         peering_name = update.name
         self.assertEqual(peering_name, "gke-ijsadoasoidasod-peer")
-    
+
     def test_update_peering(self):
         # Todo: Build a better test
         update = self.updateVpcPeering.return_updates()
